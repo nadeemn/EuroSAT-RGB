75b9904d4e1941c22df4a5b74de6f78834c0b9dc
nadeemn <nadeemnazer123@gmail.com>
2025-01-08 22:59:48 +0100

    add argparser for train test split.

--- utils/train_test_split.py
+++ utils/train_test_split.py
@@ -1,9 +1,8 @@
 import os
+import argparse
 from sklearn.model_selection import train_test_split
 
-dataset_root = r'D:\EuroSAT_RGB'
-
-def get_image_path(class_folder):
+def get_image_path(class_folder, dataset_root):
     image_paths = []
 
     for root, dirs, files in os.walk(class_folder):
@@ -13,7 +12,7 @@ def get_image_path(class_folder):
 
     return image_paths
 
-def traintest_split(dataset_root, class_names):
+def traintest_split(dataset_root, root, class_names):
     all_train_paths = []
     all_val_paths = []
     all_test_paths = []
@@ -23,8 +22,8 @@ def traintest_split(dataset_root, class_names):
     test_size = 2000
 
     for class_name in class_names:
-        class_folder = os.path.join(dataset_root, class_name)
-        images_paths = get_image_path(class_folder)
+        class_folder = os.path.join(root, class_name)
+        images_paths = get_image_path(class_folder, dataset_root)
 
         train_paths, temp_paths = train_test_split(images_paths, test_size = (1 - 0.47),
                                                     random_state = 42)
@@ -44,17 +43,27 @@ def save_splits_to_files(split_paths, split_name, output_dir):
 
 
 if __name__== "__main__":
+    parser = argparse.ArgumentParser(description="Train Test Split")
+    parser.add_argument('--root_dir', type=str, required=True, help="""Root Directory of the dataset. 
+                        (No need to give the entire directory. Only parent directory is enough.)
+                        For e.g. if the file path is: D:\EuroSAT_MS\EuroSAT_MS\AnnualCrop\AnnualCrop_1.tif.
+                        Give the root as D:\EuroSAT_MS
+                        """ )
+    args = parser.parse_args()
+    dataset_root = args.root_dir
+
     current_file = os.path.abspath(__file__)
     script_dir = os.path.dirname(current_file)
     project_root = os.path.abspath(os.path.join(script_dir, '..'))
     output_dir = os.path.join(project_root, 'splits')
 
-    root = os.path.join(dataset_root, 'EuroSAT_RGB')
+    subdir = [d for d in os.listdir(dataset_root) if os.path.isdir(os.path.join(dataset_root, d))]
+    root = os.path.join(dataset_root, subdir[0])
 
     class_names = [class_name for class_name in os.listdir(root)
                     if os.path.isdir(os.path.join(root, class_name))]
     
-    train_paths, val_paths, test_paths = traintest_split(root, class_names)
+    train_paths, val_paths, test_paths = traintest_split(dataset_root, root, class_names)
 
     train_set = set(train_paths)
     val_set = set(val_paths)
